7e79e3acf4f715db4969dab61402891925e4324f
Micro-Organism <coolrabbit1993@gmail.com>
2024-10-30 10:18:56 +0800

    feat: springboot 集成 debezium

---
+++ zero-debezium-boot/src/main/java/com/zero/debezium/ZeroDebeziumBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.debezium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroDebeziumBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroDebeziumBootApplication.class, args);
+    }
+
+}
---
+++ zero-debezium-boot/src/test/java/com/zero/debezium/ZeroDebeziumBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.debezium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroDebeziumBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
